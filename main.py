fd3827eeedc2eb30f819fd613050470ff8688efe
haziq-usman <117289025+haziq-usman@users.noreply.github.com>
2023-09-27 12:51:52 +0500

    vercel.json added

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ app = FastAPI()
 
 @app.get("/runquery")
 async def qa(query:str):
-    os.environ['OPENAI_API_KEY'] = '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'
-
     integration_token = "secret_Pl4SjrFzxjcxWs3dW3VRZudWaY4vxbhbtFmhpBfBDAW"
     page_ids = ["74b62523c28944f29340a7b8af9975ff"]
     documents = NotionPageReader(integration_token=integration_token).load_data(
@@ -19,4 +17,4 @@ async def qa(query:str):
 
     # set Logging to DEBUG for more detailed outputs
     query_engine = index.as_query_engine()
-    return {"response": query_engine.query(query).response}
+    return {"response": await query_engine.query(query).response}
