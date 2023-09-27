ccbff984b5022f055b16f1814ef7a993401f6d5e
haziq-usman <117289025+haziq-usman@users.noreply.github.com>
2023-09-27 17:34:27 +0500

    Index saved to disk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import sys
-from llama_index import SummaryIndex, NotionPageReader
+from langchain.chat_models import ChatOpenAI
+from llama_index import NotionPageReader, GPTSimpleVectorIndex, ServiceContext, LLMPredictor
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -15,14 +16,17 @@ app.add_middleware(
 )
 @app.get("/runquery")
 def qa(query:str):
-    integration_token = "secret_Pl4SjrFzxjcxWs3dW3VRZudWaY4vxbhbtFmhpBfBDAW"
-    page_ids = ["74b62523c28944f29340a7b8af9975ff"]
-    documents = NotionPageReader(integration_token=integration_token).load_data(
-        page_ids=page_ids
-    )
-    index = SummaryIndex.from_documents(documents)
-
-    # set Logging to DEBUG for more detailed outputs
-    query_engine = index.as_query_engine()
-    response = query_engine.query(query)
+    os.environ['OPENAI_API_KEY'] = "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"
+    # integration_token = "secret_V9k7V8eN9vK31E1CET4PMzeWJ6mDfpHBXiQHZAN6LPn"
+    # page_ids = ["1c4e4b7700cb4773a4df2ed8062a2ea9"]
+    # print("Starting index generatation...")
+    # llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.3, model_name="gpt-3.5-turbo", max_tokens=512))
+    # service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+    # documents =  NotionPageReader(integration_token=integration_token).load_data(
+    #     page_ids=page_ids
+    # )
+    # index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+    # index.save_to_disk("index.json")
+    index = GPTSimpleVectorIndex.load_from_disk("index.json")
+    response = index.query(query)
     return {"response": response.response}
